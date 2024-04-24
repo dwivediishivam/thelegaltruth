cd564874a9c618a1555d11a63ad197de445f5edf
Shivam Dwivedi <shivam21352@iiitd.ac.in>
2024-04-24 23:54:51 +0530

    openai connection try 1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import os
+import openai
 
 app = Flask(__name__)
 
@@ -7,6 +8,8 @@ UPLOAD_FOLDER = os.path.join('uploads')  # Using 'os.path.join' for future modif
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Create the upload directory if it doesn't exist
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/')
 def index():
@@ -26,8 +29,16 @@ def upload_file():
     return 'Invalid file type', 400
 
 def process_pdf(filepath):
+    model_name = "ft:gpt-3.5-turbo-0125"
+    response = openai.ChatCompletion.create(
+        model=model_name,
+        messages=[{"role": "system", "content": "Just return me the following line :"},
+                    {"role": "user", "content": "OPEN AI CONNECTION SUCCESFUL"}],
+        temperature=0.7
+    )
+    story = response['choices'][0]['message']['content']
 
-    return('Successful')
+    return story
 
 if __name__ == '__main__':
     app.run(debug=True)
